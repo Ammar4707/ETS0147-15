3580da978c15bcaaac31bd8b5b678fa4438842d2
Ammar4707 <mukiammar@gmai.com>
2025-03-20 18:02:36 +0300

    first

--- Python/capitilize.py
+++ Python/capitilize.py
@@ -1,3 +1,18 @@
 text = "hello world"
-capitilized_text = text.capitilize()
+capitilized_text = text.upper()
 print(capitilized_text)
+
+
+text = "MeCHatRonicS"
+lower_text = text.lower()
+print(lower_text)  # Output: "mechatronics"
+
+text = "I have a car"
+upper_text = text.upper()
+print(upper_text)  # Output: "I HAVE A CAR"
+
+text = "THE building is about to collapse"
+title_text = text.title()
+print(title_text)  # Output: "The Building Is About To Collapse"
+
+
