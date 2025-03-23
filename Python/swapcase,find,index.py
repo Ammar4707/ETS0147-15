adc2a00b5f731005a0f37bff01e7fdfbbda992ae
Ammar4707 <mukiammar@gmai.com>
2025-03-24 00:00:22 +0300

    3

--- Python/swapcase,find,index.py
+++ Python/swapcase,find,index.py
@@ -1,11 +1,11 @@
 quote = "The Car Has a dark TINT!"
 swapped_quote = quote.swapcase()
-print(swapped_quote)  
+print(swapped_quote)  # Output: tHE cAR hAS A DARK tint!
 
 shopping_list = "eggs, mangoes, unicorn feathers, garlic"
 position = shopping_list.find("unicorn feathers")
-print(position)  
+print(position)  # Output: 15
 
 car_details = "This car has a powerful engine and sleek design."
 engine_position = car_details.index("engine")
-print(engine_position)  # Output: 22 (The starting index of "engine")
+print(engine_position)  # Output: 24 
