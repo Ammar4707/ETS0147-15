1c2729c70aa68ced529cb96be972f0e2abc70774
Ammar4707 <mukiammar@gmai.com>
2025-03-23 23:56:24 +0300

    :/Users/HP/AppData/Local/Programs/Python/Python313/python.exe c:/Users/HP/ETS0147-15/Python/swapcase,find,index.py
    git add .
    git push origin mai
    :/Users/HP/AppData/Local/Programs/Python/Python313/python.exe
    c:/Users/HP/ETS0147-15/Python/swapcase,find,index.py
    & C:/Users/HP/AppData/Local/Programs/Python/Python313/python.exe
    c:/Users/HP/ETS0147-15/Python/swapcase,find,index.py

---
+++ Python/swapcase,find,index.py
@@ -0,0 +1,11 @@
+quote = "The Car Has a dark TINT!"
+swapped_quote = quote.swapcase()
+print(swapped_quote)  
+
+shopping_list = "eggs, mangoes, unicorn feathers, garlic"
+position = shopping_list.find("unicorn feathers")
+print(position)  
+
+car_details = "This car has a powerful engine and sleek design."
+engine_position = car_details.index("engine")
+print(engine_position)  # Output: 22 (The starting index of "engine")
