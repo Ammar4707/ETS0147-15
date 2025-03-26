7212a6c76bc7cac8ab223633bd1d0e05140092ea
Ammar4707 <mukiammar@gmai.com>
2025-03-26 21:48:16 +0300

    fifth

---
+++ Python/count.py
@@ -0,0 +1,11 @@
+text = "good evening"
+print(text.count("e"))
+
+text = "Rainy days feel great"
+print(text.replace("Rainy", "sunny"))
+
+
+
+name = "Ethiopia"
+population = 120_000_000
+print("The population of {} is approximately {}.".format(name, population))
