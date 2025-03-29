29c44377b8eb60a24e97b98109c602d4f832b121
Ammar4707 <mukiammar@gmai.com>
2025-03-29 23:59:50 +0300

    day7

---
+++ Python/split.py
@@ -0,0 +1,13 @@
+text = "I sold a computer yesterday"
+words= text.split()
+print(words)
+
+text = ["pen", "book", "car", "cat"]
+words = ",".join(text)
+print(words)
+
+
+a1 = "hello"
+a2 = "world"
+print(a1.isalpha())
+print(a2.isalpha())
