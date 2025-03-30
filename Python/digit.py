cceab88c348d37a4020b75c2736efa620220e88f
Ammar4707 <mukiammar@gmai.com>
2025-03-30 22:37:25 +0300

    day 8

---
+++ Python/digit.py
@@ -0,0 +1,14 @@
+str1 = "99999"
+str2 = "a9a9a9"
+str3 = "99aa88%#"
+str4 = "  "
+print("#str.isdigit")
+print(str1.isdigit())
+print(str2.isdigit())
+print("#str.isalnum")
+print(str1.isalnum())
+print(str2.isalnum())
+print(str3.isalnum())
+print("#str.isspace")
+print(str1.isspace())
+print(str4.isspace())
