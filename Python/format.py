68252ed3fb9b073a2fdecf375b76ca023a2312c6
Ammar4707 <mukiammar@gmai.com>
2025-04-02 22:30:41 +0300

    day 9

---
+++ Python/format.py
@@ -0,0 +1,5 @@
+department = "Electromechanical Engineering"
+batch = "2015 EC"
+print("I'm an {} student from the batch of {}".format(department, batch))
+print(f"I'm an {department} student from the batch of {batch}")
+print(len(department))
