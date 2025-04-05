9cbe5b549eef287563b213fe67157b36f8b92849
Ammar4707 <mukiammar@gmai.com>
2025-04-05 23:25:20 +0300

    last

---
+++ Python/append.py
@@ -0,0 +1,10 @@
+fruits = ["banana", "apple", "pinapple"]
+fruits.append("orange")
+print(fruits) 
+
+list = [23,3,41,3,4,5]
+list.clear()
+print(list)
+list2=[33,44,55,67,8]
+copied_list = list2.copy()
+print(copied_list)
