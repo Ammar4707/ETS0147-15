5edbab056150ed5fefeeb3afb3b2660c915e3c55
Ammar4707 <mukiammar@gmai.com>
2025-04-08 23:02:32 +0300

    listlast

---
+++ list/reverse.md
@@ -0,0 +1,2 @@
+the reverse() reverses the order of elements in a list in place
+the sort() sorts the list in their ascending order
---
+++ list/reverse.py
@@ -0,0 +1,11 @@
+numbers=[1,2,3,4,5,6,7]
+words=["apple","cars","books","violin","pc"]
+words.reverse()
+numbers.reverse()
+print(numbers)
+print(words)
+
+numbers.sort()
+words.sort()
+print(numbers)
+print(words)
