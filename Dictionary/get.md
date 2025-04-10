1b7cc217a8792499fca8b7302c73f0e2108b04fa
Ammar4707 <mukiammar@gmai.com>
2025-04-10 23:57:33 +0300

    3rd

---
+++ Dictionary/get.md
@@ -0,0 +1,3 @@
+the get() retrives the value for a given key
+the items() returns all key value pairs as tuples in a list like format
+the keys() returns all the keys in a dictionary
---
+++ Dictionary/get.py
@@ -0,0 +1,10 @@
+info={"name":"Ammar","department":"EME"}
+name=info.get("name")
+print(f"Name:{name}")
+
+car_colors={"toyota":"red","mercedes":"black"}
+ALL=car_colors.items()
+print(f"All cars and car colors:{ALL}")
+
+keys_list=list(info.keys())
+print(keys_list)
