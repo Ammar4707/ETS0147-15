3748b97f291f7288f77e0fd831395ccf7190cd56
Ammar4707 <mukiammar@gmai.com>
2025-04-12 23:53:28 +0300

    last

---
+++ Dictionary/update.md
@@ -0,0 +1,2 @@
+the update () updates new keyvalue pairs in a dictionary. If its missing, then it adds a new one.
+the values() shows all the values in the dictionary.
---
+++ Dictionary/update.py
@@ -0,0 +1,7 @@
+info={"name":"Ammar", "Gender":"Male"}
+info.update({"name":"Musa", "age":"23"})
+print(info)
+
+info2={"name":"Akrem","Gender":"Male","age":"18"}
+print(info2.values())
+print(list(info2.values()))
